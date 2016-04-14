f1988254008e6be2c0ffbf21f2a72a0baea6b91c
heitorbarbieri <barbieri@paho.org>
2016-04-14 16:58:02 -0300

    first codes

---
+++ src/LoadUrl.py
@@ -0,0 +1,123 @@
+#=========================================================================
+#
+#    Copyright © 2016 BIREME/PAHO/WHO
+#
+#    This file is part of API-NLM.
+#
+#    API-NLM is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public License as
+#    published by the Free Software Foundation, either version 2.1 of
+#    the License, or (at your option) any later version.
+#
+#    API-NLM is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with API-NLM. If not, see <http://www.gnu.org/licenses/>.
+#
+#=========================================================================
+
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+import urllib.parse
+
+responses = {
+    100: ('Continue', 'Request received, please continue'),
+    101: ('Switching Protocols',
+          'Switching to new protocol; obey Upgrade header'),
+
+    200: ('OK', 'Request fulfilled, document follows'),
+    201: ('Created', 'Document created, URL follows'),
+    202: ('Accepted',
+          'Request accepted, processing continues off-line'),
+    203: ('Non-Authoritative Information', 'Request fulfilled from cache'),
+    204: ('No Content', 'Request fulfilled, nothing follows'),
+    205: ('Reset Content', 'Clear input form for further input.'),
+    206: ('Partial Content', 'Partial content follows.'),
+
+    300: ('Multiple Choices',
+          'Object has several resources -- see URI list'),
+    301: ('Moved Permanently', 'Object moved permanently -- see URI list'),
+    302: ('Found', 'Object moved temporarily -- see URI list'),
+    303: ('See Other', 'Object moved -- see Method and URL list'),
+    304: ('Not Modified',
+          'Document has not changed since given time'),
+    305: ('Use Proxy',
+          'You must use proxy specified in Location to access this '
+          'resource.'),
+    307: ('Temporary Redirect',
+          'Object moved temporarily -- see URI list'),
+
+    400: ('Bad Request',
+          'Bad request syntax or unsupported method'),
+    401: ('Unauthorized',
+          'No permission -- see authorization schemes'),
+    402: ('Payment Required',
+          'No payment -- see charging schemes'),
+    403: ('Forbidden',
+          'Request forbidden -- authorization will not help'),
+    404: ('Not Found', 'Nothing matches the given URI'),
+    405: ('Method Not Allowed',
+          'Specified method is invalid for this server.'),
+    406: ('Not Acceptable', 'URI not available in preferred format.'),
+    407: ('Proxy Authentication Required', 'You must authenticate with '
+          'this proxy before proceeding.'),
+    408: ('Request Timeout', 'Request timed out; try again later.'),
+    409: ('Conflict', 'Request conflict.'),
+    410: ('Gone',
+          'URI no longer exists and has been permanently removed.'),
+    411: ('Length Required', 'Client must specify Content-Length.'),
+    412: ('Precondition Failed', 'Precondition in headers is false.'),
+    413: ('Request Entity Too Large', 'Entity is too large.'),
+    414: ('Request-URI Too Long', 'URI is too long.'),
+    415: ('Unsupported Media Type', 'Entity body in unsupported format.'),
+    416: ('Requested Range Not Satisfiable',
+          'Cannot satisfy request range.'),
+    417: ('Expectation Failed',
+          'Expect condition could not be satisfied.'),
+
+    500: ('Internal Server Error', 'Server got itself in trouble'),
+    501: ('Not Implemented',
+          'Server does not support this operation'),
+    502: ('Bad Gateway', 'Invalid responses from another server/proxy.'),
+    503: ('Service Unavailable',
+          'The server cannot process the request due to a high load'),
+    504: ('Gateway Timeout',
+          'The gateway server did not receive a timely response'),
+    505: ('HTTP Version Not Supported', 'Cannot fulfill request.'),
+}
+
+def loadUrl(url,
+            header = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' },
+            post_values = None, # if None => GET else POST
+            post_values_encod = 'ascii'):
+    """
+        url - the internet address of the content to be downloaded
+        header - dictionary of the http send header
+        post_values - dictionary of the POST parameters. If None is used the
+                      send protocol used would be GET
+        post_values_encod - the encoding used in post_values dictionary
+        Returns the download content of a internet resource
+    """
+
+    if post_values == None:
+        data = None
+    else:
+        data = urllib.parse.urlencode(post_values)
+        data = data.encode(post_values_encod)
+
+    req = Request(url, data, header)
+
+    try:
+        response = urlopen(req)
+        content = response.read()
+    except HTTPError as e:
+        resp = (e.code, responses[e.code][0])
+    except URLError as e:
+        resp = (-1, e.reason)
+    else:
+        resp = (200, content)
+
+    return resp
---
+++ src/MongoDb.py
@@ -0,0 +1,123 @@
+#=========================================================================
+#
+#    Copyright © 2016 BIREME/PAHO/WHO
+#
+#    This file is part of API-NLM.
+#
+#    API-NLM is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public License as
+#    published by the Free Software Foundation, either version 2.1 of
+#    the License, or (at your option) any later version.
+#
+#    API-NLM is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with API-NLM. If not, see <http://www.gnu.org/licenses/>.
+#
+#=========================================================================
+
+import pymongo
+from pymongo import MongoClient
+
+class MyMongo:
+    def __init__(self, database, collection, host="localhost", port=27017):
+        """
+        database - the mongo database name
+        collection - the collection name
+        host - url of the mongo server host
+        port - mongo server port
+        """
+        self.client = MongoClient(host, port)
+        self.db = self.client[database]
+        self.col = self.db[collection]
+
+    def saveDoc(self, doc):
+        """
+        Saves a new document into the selected collection
+        doc - mongo document represented as a dictionary
+        """
+        self.col.insert_one(doc)
+
+    def replaceDoc(self, doc):
+        """
+        Replaces an exiting saved document by a ney one with the same _id field
+        doc - mongo document represented as a dictionary
+        Returns True if the replacement was ok or False if not
+        """
+        _id = {'_id' : doc['_id']}
+        return (self.col.replace_one(_id, doc).modified_count == 1)
+
+    def loadDoc(self, id):
+        """
+        id - mongo document _id field
+        Returns a saved document
+        """
+        return self.col.find_one(id)
+
+    def loadDocs(self, query = {}):
+        """
+        query - mongo document pattern represented as a dictionary to be used
+                as a filter
+        Returns an ordered pair of type (<size of the list>,<list of documents>)
+        """
+        cursor = self.col.find(query)
+        return (cursor.count(), cursor)
+
+    def deleteDoc(self, id):
+        """
+        Deletes a document from the collection
+        id - mongo document _id field
+        Returns True if the deletion was ok or False if not
+        """
+        idd = {'_id' : id}
+        return (self.col.delete_one(idd).deleted_count == 1)
+
+    def numOfDocs(self):
+        """
+        Returns the number of documents of this collection
+        """
+        return self.col.count()
+
+    def createIndex(self, name, fieldNames):
+        """
+        Creates an index for this collection
+        name - the index name
+        fieldNames - list of mongo document fields to be indexed
+        """
+
+        flds = []
+        for fname in fieldNames:
+            flds.append((fname, pymongo.ASCENDING))
+        self.col.create_index(flds, name=name)
+
+    def dropIndex(self, name):
+        """
+        Deletes an index
+        name - the index name
+        """
+        self.col.drop_index(name)
+
+    def reindexAll(self):
+        """
+        Reindex all indexes
+        """
+        self.col.reindex()
+
+    def listIndexes(self):
+        """
+        Returns a list of names of the collection indexes
+        """
+        idx = []
+        for index in self.col.list_indexes():
+            idx.append(index['name'])
+        return idx
+
+    def dropCollection(self):
+        """
+        Deletes the collection from the database
+        """
+
+        self.db.drop_collection(self.col)
